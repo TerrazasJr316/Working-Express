115e3490f5acbfc20533be728181d9f2cb1e6969
TerrazasJr316 <terrazasjosue0@gmail.com>
2026-06-04 11:08:16 -0600

    feat(auth) implentación sel servicio auth

--- BackEnd-Working-Express/src/app.js
+++ BackEnd-Working-Express/src/app.js
@@ -2,21 +2,20 @@ const express = require('express');
 const cors = require('cors');
 const { errorHandler } = require('./middlewares/errorHandler');
 
+const authRoutes = require('./routes/auth.routes');
+
 const app = express();
 
-// Middlewares globales
-app.use(cors()); // Permite peticiones de otros dominios/puertos
-app.use(express.json()); // Entiende los datos que lleguen en formato JSON (req.body)
+app.use(cors()); 
+app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
 
-// Ruta de prueba inicial
 app.get('/', (req, res) => {
-    res.json({ message: "API de Working Express funcionando" });
+    res.json({ message: "API de Working Express funcionando correctamente" });
 });
 
-// AQUI IRÁN TUS RUTAS EN EL FUTURO (ej. app.use('/api/users', userRoutes))
+app.use('/api/auth', authRoutes);
 
-// Middleware de manejo de errores (DEBE IR SIEMPRE AL FINAL DE TODO)
 app.use(errorHandler);
 
 module.exports = app;
